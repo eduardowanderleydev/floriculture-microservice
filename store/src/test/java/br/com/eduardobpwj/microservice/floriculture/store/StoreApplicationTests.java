0af23bd4aae5278770cf9c6a0aeadfd859cb9542
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-11-17 15:29:49 -0300

    Initial commit.

---
+++ store/src/test/java/br/com/eduardobpwj/microservice/floriculture/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardobpwj.microservice.floriculture.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
