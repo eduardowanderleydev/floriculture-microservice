7809e4d92ace64227c78ad7bec00b3527b7676af
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-11-17 18:10:53 -0300

    Implementing service discovery with Eureka

---
+++ eureka-server/src/test/java/br/com/eduardobpwj/microservice/floriculture/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardobpwj.microservice.floriculture.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ provider/src/test/java/br/com/eduardobpwj/microservice/floriculture/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardobpwj.microservice.floriculture.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
