7f59e74ad22a85487aa7a7b81110098b8c352596
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-11-17 18:54:56 -0300

    Starting to implement configuration server.

---
+++ config-server/src/test/java/br/com/eduardobpwj/microservice/floriculture/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardobpwj.microservice.floriculture.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
