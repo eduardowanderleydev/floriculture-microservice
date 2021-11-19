09596b32a9c058243f796f0b0cb8bb671a21ff2e
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-11-19 18:26:41 -0300

    Implementig API gateway zuul.

---
+++ zuul/src/test/java/br/com/eduardobpwj/microservice/floriculture/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardobpwj.microservice.floriculture.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
